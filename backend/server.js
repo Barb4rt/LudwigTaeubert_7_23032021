36b304efdc291e065116efe16d8c7706c657cd1b
Ludwig Taeubert <ludwig.taeuber@gmail.com>
2021-06-09 12:52:20 +0200

    Fin du projet

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const apiRouter = require ('./apiRouter').router;
 const path = require('path');
 const server = express();
 
+const PORT = 8081
+
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100 // limit each IP to 100 requests per windowMs
@@ -27,6 +29,6 @@ server.use('/api/', apiRouter);
 server.use('/images', express.static(path.join(__dirname, 'images')));
 server.use('/images/profilesPicture', express.static(path.join(__dirname, 'images/profilesPicture')));
 
-server.listen(8081, function(){
-    console.log('serveur en écoute');
+server.listen(PORT, function(){
+    console.log(`serveur en écoute sur le port : ${PORT}`);
 })
